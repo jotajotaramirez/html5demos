1c379a0329460a0a2a2b692692c4a9e0e6054d10
Juan José Ramírez <juanjose.ramirez@gmail.com>
2013-07-07 19:22:01 +0200

    Sun shadow

--- js/sunShadow.js
+++ js/sunShadow.js
@@ -5,6 +5,8 @@
 
 $(function() {
 	var NUM_CELLS = 10;
+	var SHADOW_OFFSET_MULTIPLER = 5;
+
 	var Cell = function(x, y, backgroundColor) {
 		this.x = x;
 		this.y = y;
@@ -17,16 +19,33 @@ $(function() {
 				this.DOMelement = $(document.createElement('div')).addClass('cell');
 				parent.append(this.DOMelement);
 			}
+			var distanceFromSun = {
+				x: ((this.x / 100) - 0.5),
+				y: ((this.y / 100) - 0.5),
+				absolute: function() {
+					return Math.abs(this.x * this.y);
+				}
+			}
+
+			var shadow = (((this.x / 100) - 0.5) * SHADOW_OFFSET_MULTIPLER) + 'em ' +
+						(((this.y / 100) - 0.5) * SHADOW_OFFSET_MULTIPLER) + 'em ' +
+						(distanceFromSun.absolute() * 20) + 'em rgba(0, 0, 0, 0.8)' +
+					', inset ' +
+						(0.5 - (this.x / 100)) + 'em ' +
+						(0.5 - (this.y / 100)) + 'em ' +
+						'1em rgba(0, 0, 0, 0.8)';
+
 			this.DOMelement.css('left', this.x + "%")
 				.css('top', this.y + "%")
-				.css('background-color', this.backgroundColor);
+				.css('background-color', this.backgroundColor)
+				.css('box-shadow', shadow);
 		}
 	}
 
 	var cells = [];
 	var main = $('.main');
 	for (var i = 0; i < NUM_CELLS; i++) {
-		var cell = new Cell(0, 0, randomColor());
+		var cell = new Cell(0, 0, 'rgba(' + randomRange(100, 255) + ', 0, 0, 1.0)');
 		cell.render(main);
 		cells.push(cell);
 	}
@@ -34,7 +53,6 @@ $(function() {
 	var moveCell = function(cell) {
 		cell.x = randomRange(0, 95);
 		cell.y = randomRange(0, 95);
-		cell.backgroundColor = randomColor();
 		cell.render();
 		setTimeout(function() {
 			moveCell(cell);
