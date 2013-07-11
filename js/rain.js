c8ce5daa03b61ed5d074eb206dbb82455cb3ee78
Juan José Ramírez <juanjose.ramirez@gmail.com>
2013-07-11 12:25:16 +0200

    Rain new version

--- js/rain.js
+++ js/rain.js
@@ -6,35 +6,76 @@
 $(function() {
 	var canvas = $('canvas');
 	var context = canvas.get(0).getContext('2d');
-	var NUM_DROPS = 100;
+	var NUM_DROPS = canvas.width();
+	var wind = 0;
 
-	var Drop = function(x, y, speedX, speedY) {
+	var Drop = function(x, y, speedX, maxFall) {
 		this.x = x;
 		this.y = y;
 		this.speedX = speedX;
-		this.speedY = speedY;
-		this.height = speedY * 2;
+		this.maxFall = maxFall;
+		this.dropRadius = 0;
+		this.step = 0;
+		this.windAcceleration = 0;
 	}
 
 	Drop.prototype = {
 		'update': function(container) {
-			this.x = (this.x + this.speedX) % 100;
-			this.y = (this.y + this.speedY) % 100;
+			if (this.y < this.maxFall) {
+				this.speedY = 9.8 / 3000 * this.step;
+				this.height = this.speedY * 2;
+				this.x = (this.x + this.speedX + this.windAcceleration) % 100;
+				if (this.x < 0) {
+					this.x = 100 - this.x;
+				}
+				this.step++;
+				this.y += this.speedY;
+				this.windAcceleration += (wind / Math.max(this.windAcceleration, 0.1)) / 10000; // this can be improved
+ 			}
+			
+			if (this.y + this.height >= this.maxFall) {
+				this.dropRadius += this.speedY / 10;
+
+				if (this.dropRadius > 2) { // reset
+					this.y = randomRange(-500, 0);
+					this.dropRadius = 0;
+					this.step = 0;
+				}
+			}
 		},
-		'paint': function(canvas) {
-			var context = canvas.get(0).getContext('2d');
+		'paint': function(canvas, context, width, height) {
 			context.beginPath();
-			context.moveTo((this.x / 100) * canvas.width(), (this.y / 100) * canvas.height());
-			context.lineTo((this.x / 100) * canvas.width(), (this.y + this.height) * canvas.height() / 100);
-			context.closePath();
-			context.stroke();
+
+			var xFromPos = this.x * width / 100,
+				xToPos = (this.x + this.speedX + this.windAcceleration) * width / 100,
+				yFromPos = this.y * height / 100,
+				yToPos = Math.min(this.y + this.height, this.maxFall) * height / 100;
+
+			if (this.y < this.maxFall) {
+				context.strokeStyle = 'rgba(255, 255, 255, 1.0)';
+				context.moveTo(xFromPos, yFromPos);
+				context.lineTo(xToPos, yToPos);
+				context.closePath();
+				context.stroke();
+			}
+
+			if (this.y + this.height >= this.maxFall) {
+				context.strokeStyle = 'rgba(255, 255, 255, ' + (1 - (this.dropRadius / 2)) + ')';
+				context.save();
+				context.scale(1, 0.2);
+				context.beginPath();
+				context.arc(xToPos, yToPos * 5, Math.pow(this.maxFall / 100, 8) * this.dropRadius * width / 100, 0, 2 * Math.PI);
+				context.closePath();
+				context.restore();
+				context.stroke();
+			}
 		}
 	}
 
 	var drops = [];
 
 	for (var i = 0; i < NUM_DROPS; i++) {
-		var drop = new Drop(randomNumber(100), 0, randomRangeFloat(0, 0.02), randomRangeFloat(0.2, 1.0));
+		var drop = new Drop(randomNumber(100), randomRange(-500, 0), 0, randomRange(80, 100));
 		drops.push(drop);
 	}
 
@@ -42,13 +83,13 @@ $(function() {
 		var width = canvas.width();
 		var height = canvas.height();
 		
-		context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+		context.fillStyle = 'rgba(0, 0, 0, 0.2)'; // alpha for a blur effect
 		context.fillRect(0, 0, width, height);
-		context.strokeStyle = '#fff';
+		
 		for (var i = 0; i < drops.length; i++) {
 			var drop = drops[i];
 			drop.update(canvas);
-			drop.paint(canvas);
+			drop.paint(canvas, context, width, height);
 		}
 	}
 
@@ -67,4 +108,11 @@ $(function() {
 	}
 
 	drawLoop(0);
+
+	var changeWind = function() {
+		wind = randomRangeFloat(-0.3, 0.3);
+		setTimeout(changeWind, randomRange(1000, 10000));
+	};
+
+	setTimeout(changeWind, 3000);
 });
